915187165c0b32eb21e4e90b54c8681d524b23f4
Vinícius Quadrado <68359941+viniciuSquare@users.noreply.github.com>
2021-04-18 10:15:12 -0300

    Filters implemented, code cleaner - next to do -> get props of a range time - improve time formatting

--- src/logFormatter.js
+++ src/logFormatter.js
@@ -5,15 +5,27 @@ function splitter(content, pattern){
 }
 
 const originaHeader = splitter(`date time s-ip cs-method cs-uri-stem cs-uri-query s-port cs-username c-ip cs(User-Agent) cs(Referer) sc-status sc-substatus sc-win32-status time-taken`, " ")
-
-const header = splitter(`s-ip cs-uri-query c-ip sc-status time-taken`, " ")
-exports.header = header 
+const formatTime = time => {
+    time = splitter(time, ':')
+    if(time[0] == '02')
+        time[0] = '23'
+    else if( time[0] == '01' )
+        time[0] = '22'
+    else if( time[0] == '00' )
+        time[0] = '21'
+    else
+        time[0] = time[0]-3 
+    return time.join(':')
+}
 
 function logToObject(log){
     let aux = new Object
     
     originaHeader.forEach((head, i)=>{
         switch(head){
+            case 'time':
+                aux[head] = formatTime(log[i])
+                break;
             case 's-ip':
             case 'c-ip':
             case 'cs-uri-stem':
@@ -27,6 +39,20 @@ function logToObject(log){
     return aux
 }
 
+// const header = splitter(`s-ip cs-uri-query c-ip sc-status time-taken`, " ")
+
+// Filter rules and operations  
+const filterByMethod = (e) => e['cs-uri-stem']
+const filterClients = (e) => e['c-ip']
+
+const sum = (accumulator, currentValue) => accumulator + currentValue;
+const groupBy = key => array =>
+    array.reduce((objectsByKeyValue, obj) => {
+        const value = obj[key];
+        objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
+        return objectsByKeyValue;
+    }, {});
+
 function logFormatter(logFile){
     let logsBruto = splitter(logFile, "\n")
     const logs = []
@@ -35,28 +61,31 @@ function logFormatter(logFile){
         log = splitter(log, " ")
 
         //jumping the headers
-        if(log[0][0] !== "#")
-            logs.push(logToObject(log))
-    })
+        switch(log[0][0]){
+            case("#"):
+                break;
+            
+            default:
+                logs.push(logToObject(log))
+                break;
 
+        }
+    })
     logs.pop()
 
-    const filterByMethod = (e) => e['cs-uri-stem']
+// Filter methods
     let methods = logs.map(filterByMethod)
     
     //Remove repeated methods
     methods = Array.from(new Set(methods))
     
-    const methodsTimes = []
-    
-    const sum = (accumulator, currentValue) => accumulator + currentValue;
-    
+    const methodsTimes = []   
     methods.forEach(method => {
         let times = []
         methodsTimes[method] = [times]
     })
     
-    //calc the avarage time to each method    
+//calc the avarage time to each method    
     logs.forEach(log => {
         methodsTimes[log['cs-uri-stem']] ? 
             methodsTimes[log['cs-uri-stem']][0].push(+log['time-taken']) 
@@ -75,15 +104,23 @@ function logFormatter(logFile){
     methods.forEach(method => 
         avarageByMethod[method] = methodsTimes[method].avarage)
 
-    // Clients connected to the API
-    const filterByClient = (e) => e['c-ip']
-    let clients = logs.map(filterByClient)
+// Clients
+    // Connected to the API
+    let clients = logs.map(filterClients)
 
     //Remove repeated clients
     clients = Array.from(new Set(clients))
 
-    return { logsBruto, logs, methods, methodsTimes, avarageByMethod, clients}
+    // Methods / client
+    const operationsClient = groupBy('c-ip')(logs)
+
+    // ['172.168.3.46'].map(ops => ops['cs-uri-stem'] == '/v1/finalizarVenda'))
+
+    //times
+
+    console.log(logs)
+    const header = Object.keys(logs[0])
+    return { logsBruto, logs, methods, methodsTimes, avarageByMethod, clients, header}
 
 }
-
 exports.logFormatter = logFormatter;
--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ const fs = require("fs")
 const { logFormatter } = require('./logFormatter')
 
 //log file path
-const path = "./logs/data.log"
+const path = "./data/data.txt"
 const conteudo = fs.readFileSync(path, 'utf-8')
 
 //destruct variables returned by the function
 const {logs, methods, methodsTimes, avarageByMethod, clients} = logFormatter(conteudo)
 
-console.log(avarageByMethod)
+// console.log(logs)
 // console.log(avarageByMethod)
 
 exports.logs = logs
